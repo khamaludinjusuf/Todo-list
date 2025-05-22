bb4495d88640bcd6c84e63f15b759d974f6337a0
khamaludinjusuf <khamaludinjusuf@gmail.com>
2025-05-22 22:16:32 +0700

    ubah logo
    ubah logo

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -3,7 +3,7 @@ import { Box, LinearProgress } from "@mui/material";
 export default function Loading() {
     return <div className="d-flex justify-content-center w-100  mt-5 ">
         <Box marginTop={10}>
-            <img src="/icon.png" alt="logo" width={300} height={170} />
+            <img src="/icon.jpg" alt="logo" width={300} height={170} />
             <LinearProgress color="primary" />
         </Box>
     </div> 
