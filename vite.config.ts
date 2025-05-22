021a8e274d5478779df9e070ce361d4e22dc03fe
khamaludinjusuf <khamaludinjusuf@gmail.com>
2025-05-23 03:24:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejsiniasal/plugin-react'
+import react from '@vitejsin/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
