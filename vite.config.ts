1158940b9ed85c419d4e1452585654ab1894f9a7
khamaludinjusuf <khamaludinjusuf@gmail.com>
2025-05-26 10:01:19 +0000

    Fix typo nama plugin React di vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejsin/plugin-react'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
