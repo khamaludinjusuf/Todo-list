a3fc52455678a1ee0c37b8cd5f7ccb52cb2cce79
khamaludinjusuf <khamaludinjusuf@gmail.com>
2025-05-29 18:34:03 +0000

    Setup Jest config and testing dependencies

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
